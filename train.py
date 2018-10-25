c06b4cad17542e1a4efe6b8a82a0f502361f0979
JC Li <jincheng.li@gmail.com>
2018-10-25 15:16:27 -0700

    Modify the template name.

--- train.py
+++ train.py
@@ -44,7 +44,7 @@ def main(config, resume):
     trainer.train()
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='PyTorch Template')
+    parser = argparse.ArgumentParser(description='PyTorch Visual Perception')
     parser.add_argument('-c', '--config', default=None, type=str,
                            help='config file path (default: None)')
     parser.add_argument('-r', '--resume', default=None, type=str,
